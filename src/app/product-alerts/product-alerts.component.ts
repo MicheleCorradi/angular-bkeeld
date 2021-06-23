3106db9530a7b15567ac53ea0d1834ecd1317b51
MicheleCorradi <michelecorradik@gmail.com>
2021-06-23 14:52:46 +0200

    1 file added

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -5,8 +5,8 @@ import { Product } from '../products';
 
 @Component({
   selector: 'app-product-alerts',
-  templateUrl: './product-alerts.component.html',
-  styleUrls: ['./product-alerts.component.css']
+  templateUrl: '.product-alerts.component.html',
+  styleUrls: ['product-alerts.component.css']
 })
 export class ProductAlertsComponent {
   @Input() product: Product|undefined;
