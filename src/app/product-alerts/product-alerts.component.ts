da70b944faaea439033e5d23fcc2c76840c02cb3
MicheleCorradi <michelecorradik@gmail.com>
2021-06-24 17:06:16 +0200

    data management

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -5,7 +5,7 @@ import { Product } from '../products';
 
 @Component({
   selector: 'app-product-alerts',
-  templateUrl: '.product-alerts.component.html',
+  templateUrl: 'product-alerts.component.html',
   styleUrls: ['product-alerts.component.css']
 })
 export class ProductAlertsComponent {
